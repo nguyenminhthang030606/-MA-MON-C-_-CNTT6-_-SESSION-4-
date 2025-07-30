cf0439e04a9848fe50e8eaf44df91fc782a895b8
nguyenminhthang030606 <nmthang030606@gmail.com>
2025-07-30 22:30:14 +0700

    Update [MON C]_[CNTT6]_[SESSION3]_[B9].c

--- ss4/[MON C]_[CNTT6]_[SESSION3]_[B9].c
+++ ss4/[MON C]_[CNTT6]_[SESSION3]_[B9].c
@@ -1,29 +1,6 @@
 #include <stdio.h>
-
-int isLeapYear(int year) {
-    return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
-}
-
-int isValidDate(int day, int month, int year) {
-    if (year < 1 || month < 1 || month > 12 || day < 1) return 0;
-    int daysInMonth;
-    switch (month) {
-        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-            daysInMonth = 31; break;
-        case 4: case 6: case 9: case 11:
-            daysInMonth = 30; break;
-        case 2:
-            daysInMonth = isLeapYear(year) ? 29 : 28; break;
-        default:
-            return 0;
-    }
-
-    return (day <= daysInMonth);
-}
-
 int main() {
-    int day, month, year;
-
+    int year, month, day;
     printf("Nhap ngay: ");
     scanf("%d", &day);
     printf("Nhap thang: ");
@@ -31,10 +8,38 @@ int main() {
     printf("Nhap nam: ");
     scanf("%d", &year);
 
-    if (isValidDate(day, month, year)) {
-        printf("Ngay %02d/%02d/%d la ngay hop le.\n", day, month, year);
-    } else {
-        printf("Ngay %02d/%02d/%d KHONG phai la ngay hop le.\n", day, month, year);
+    if ((year<0) || (month<0||month>12)||(day<0||day>31)) {
+        printf(" ko hop le");
+        return 1;
+    }
+
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        printf("%d Day la nam nhuan",year);
+    }else {
+        printf("%d Day ko phai nam nhuan",year);
+    }
+    switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+            printf("%d co 31 ngay",month);
+            break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+            printf("%d co 30 ngay",month);
+            break;
+            case 2:
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                printf("co 29 ngay");
+            }else {
+                printf("co 28 ngay");
+            }break;
     }
 
     return 0;

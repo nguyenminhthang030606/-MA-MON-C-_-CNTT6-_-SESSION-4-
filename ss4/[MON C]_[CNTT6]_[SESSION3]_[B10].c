cf92effee1bc952d19284f7cd01e22ad4a128313
nguyenminhthang030606 <nmthang030606@gmail.com>
2025-07-30 22:33:52 +0700

    Add files via upload

---
+++ ss4/[MON C]_[CNTT6]_[SESSION3]_[B10].c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, temp;
+    printf("Nhap 3 so nguyen:\n");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a > b) {
+        temp = a; a = b; b = temp;
+    }
+    if (a > c) {
+        temp = a; a = c; c = temp;
+    }
+    if (b > c) {
+        temp = b; b = c; c = temp;
+    }
+    printf("Thu tu tang dan: %d %d %d\n", a, b, c);
+    return 0;
+}
